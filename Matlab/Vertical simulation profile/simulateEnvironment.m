39a74cb9ab09177e324f4961451a4455b38af282
MatthewMunks <matthewmunks@gmail.com>
2021-02-06 16:01:28 +0000

    Sim working2

--- Matlab/Vertical simulation profile/simulateEnvironment.m
+++ Matlab/Vertical simulation profile/simulateEnvironment.m
@@ -1,20 +1,24 @@
 function simulateEnvironment()
 
-    maxTime = 50;
-    dt = 0.1;       %s
+    maxTime = 3;        %s
+    dt = 0.001;         %s
     time = 0:dt:maxTime;
-    x = zeros(length(time),1);
+    x = zeros(length(time),1) - 0.8;
     v = zeros(length(time),1);
     a = zeros(length(time),1);
     
-    
+    x(1) = 0;
     
     for i = 2:length(time)
         omega = v(i-1) / getSpoolRadius();
         current = controller(omega);  
         theta = x(i-1) /(2*pi* getSpoolRadius());
+        
+        if (x(i-1) < -0.8)
+            break;
+        end
 
-        a(i) = getAcceleration_ForceB(omega, theta, current, x(i), -0.08);
+        a(i) = getAcceleration_ForceB(omega, theta, current);
         v(i) = getVelocity_NextInterval(v(i-1), dt, a(i));
         x(i) = getDistance_NextInterval(x(i-1), dt, v(i));
     end
